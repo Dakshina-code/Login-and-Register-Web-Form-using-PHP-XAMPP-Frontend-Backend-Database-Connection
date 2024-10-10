544828449dcc25a02485073fe74c003943c9d671
Tharindu Dakshina <tharindudakshina2004@gmail.com>
2024-10-10 15:22:13 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "+LH,l$[(F#TB$1H_)(OUaBM3";
+$db = "login";
+$conn = new mysqli($host,$user,$pass,$db);
+if ($conn->connect_error) {
+    echo"Failed to connect DB".$conn->connect_error;
+}
+
+?>
